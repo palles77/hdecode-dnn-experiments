4ed179787d5dee4ded104cb662b31b182a88e775
Lechu <leslaw.pawlaczyk@gmail.com>
2017-02-17 23:36:33 +0000

    Add files via upload

--- decode.dnn.sh
+++ decode.dnn.sh
@@ -1,1 +1,1 @@
-HDecode -A -D -T 10 -V -H hmmdefs.dnn -S list.scp -t 220.0 220.0 -C config.hdecode -i recout.mlf.dnn -w PLPL-v7.0.bg -p 0.0 -s 5.0 PLPL-v7.0.dct tiedlist.cross
+./HDecode -A -D -T 10 -V -H hmmdefs.dnn -S list.scp -t 220.0 220.0 -C config.hdecode.dnn -i recout.mlf.dnn -w PLPL-v7.0.bg -p 0.0 -s 12.0 PLPL-v7.0.dct tiedlist.cross
