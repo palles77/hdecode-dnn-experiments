7189642682f260139f0f3c1c198ced4aafe59eb2
palles77 <leslaw.pawlaczyk@gmail.com>
2017-02-19 16:04:54 +0000

    Final version with models

---
+++ README.md
@@ -0,0 +1,4 @@
+# hdecode-dnn-experiments
+Experiments in HTK DNN decoding
+
+Initial results show clear superiority of DNN vs GMM
---
+++ decode.dnn.sh
@@ -0,0 +1,3 @@
+tar -xvf hmmdefs.dnn.tar.gz
+HCopy -A -D -T 1 -C wav_config -S codetest.scp
+HDecode -A -D -T 10 -V -H hmmdefs.dnn -S list.scp -t 220.0 220.0 -C config.hdecode.dnn -i recout.mlf.dnn -w PLPL-v7.0.bg -p 0.0 -s 12.0 PLPL-v7.0.dct tiedlist.cross
