35ea663f99e0368a9ab74113e75aee0aed49f5c0
Lechu <leslaw.pawlaczyk@gmail.com>
2017-02-19 14:15:17 +0000

    Add files via upload

--- decode.gmm.sh
+++ decode.gmm.sh
@@ -1,1 +1,2 @@
+HCopy -A -D -T 1 -C wav_config -S codetest.scp
 HDecode -A -D -T 10 -V -H hmmdefs.gmm -S list.scp -t 220.0 220.0 -C config.hdecode.gmm -i recout.mlf.gmm -w PLPL-v7.0.bg -p 0.0 -s 5.0 PLPL-v7.0.dct tiedlist.cross
